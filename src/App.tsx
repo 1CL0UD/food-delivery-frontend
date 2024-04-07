050909461a7af523ce21a96485bedc8c4e7bab47
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-08 02:01:13 +0700

    added 80% width

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from './components/Navbar/Navbar';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-5/6 m-auto">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
