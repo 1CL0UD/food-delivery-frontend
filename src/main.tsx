8beb7f183ab149c55f64119e64b238d4b35fe77d
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-09 04:20:05 +0700

    moved routes to app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Cart from './pages/Cart/Cart';
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/cart',
-    element: <Cart />,
-  },
-  {
-    path: '/order',
-    element: <PlaceOrder />,
-  },
-]);
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
