9e14caf4d2892527b437d710b3ce2db826f75834
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-09 04:20:10 +0700

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Cart from './pages/Cart/Cart';
+import Home from './pages/Home/Home';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '/order',
+    element: <PlaceOrder />,
+  },
+]);
 
 const App = () => {
   return (
     <div className="w-5/6 m-auto">
       <Navbar />
+      <RouterProvider router={router} />
     </div>
   );
 };
