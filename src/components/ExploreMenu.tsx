9df4c031e1636e40632bce81ae30a1d908f1badf
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-10 04:54:40 +0700

    done making explore menu

--- src/components/ExploreMenu.tsx
+++ src/components/ExploreMenu.tsx
@@ -1,6 +1,11 @@
 import { menu_list } from '../assets/assets';
 
-const ExploreMenu = () => {
+interface Props {
+  category: string;
+  setCategory: (category: string) => void;
+}
+
+const ExploreMenu = ({ category, setCategory }: Props) => {
   return (
     <div className="explore-menu flex flex-col gap-5" id="explore-menu">
       <h1 className="text-gray-800 font-medium">Explore Our Menu</h1>
@@ -10,11 +15,17 @@ const ExploreMenu = () => {
       <div className="explore-menu-list flex justify-between items-center gap-8 text-center my-5 mx-0 overflow-x-scroll no-scrollbar">
         {menu_list.map((item, index) => {
           return (
-            <div key={index} className="explore-menu-list-item">
+            <div
+              onClick={() => setCategory(item.menu_name)}
+              key={index}
+              className="explore-menu-list-item"
+            >
               <img
                 src={item.menu_image}
                 alt={item.menu_name}
-                className="min-w-20 w-[7.5vw] cursor-pointer rounded-3xl transition"
+                className={`${
+                  category == item.menu_name ? 'active' : ''
+                } min-w-20 w-[7.5vw] cursor-pointer rounded-3xl transition`}
               />
               <p className="mt-2 text-gray-800 text-lg cursor-pointer">
                 {item.menu_name}
@@ -23,7 +34,7 @@ const ExploreMenu = () => {
           );
         })}
       </div>
-      <hr className="my-2 mx-0 h-[0.125rem] bg-slate-200 border-none" />
+      <hr className="my-2 mx-0 h-0.5 bg-slate-200 border-none" />
     </div>
   );
 };
