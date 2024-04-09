2f6f437dee389abe3c952ca1e43211b059ab2e91
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-10 05:24:44 +0700

    wrapped app with store provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import StoreContextProvider from './providers/StoreProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
   </React.StrictMode>
 );
