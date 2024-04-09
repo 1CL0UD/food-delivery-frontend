5aa21da2039234e6f65e31089f12bb15431e2172
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-10 04:54:53 +0700

    added explore menu to homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
+import ExploreMenu from '../../components/ExploreMenu';
 import Header from '../../components/Header';
 
 const Home = () => {
+  const [category, setCategory] = useState('All');
+
   return (
     <div>
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 };
