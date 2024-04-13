1836a234e0c7a83a7b960096d11257fca0dfeac1
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-14 04:41:55 +0700

    food items now shown

---
+++ src/components/FoodItem.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { assets } from '../assets/assets';
+import { FoodList } from '../providers/StoreProvider';
+
+interface Props {
+  item: FoodList;
+}
+const FoodItem = ({ item }: Props) => {
+  const [itemCount, setItemCount] = useState(0);
+  return (
+    <div className="food-item w-full m-auto rounded-2xl shadow-lg animate-fadein">
+      <div className="food-item-img-container relative">
+        <img
+          className="food-item-image rounded-t-2xl w-full"
+          src={item.image}
+          alt=""
+        />
+        {!itemCount ? (
+          <img
+            className="add absolute w-10 bottom-4 right-4 cursor-pointer rounded-full transition ease-in-out hover:-translate-x-1"
+            onClick={() => setItemCount((prev) => prev + 1)}
+            src={assets.add_icon_white}
+          />
+        ) : (
+          <div className="food-item-counter absolute bottom-4 right-4 flex items-center gap-3 p-1 rounded-full bg-white">
+            <img
+              className="w-8 cursor-pointer"
+              onClick={() => setItemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+            />
+            <p>{itemCount}</p>
+            <img
+              className="w-8 cursor-pointer"
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+            />
+          </div>
+        )}
+      </div>
+      <div className="food-item-info p-5">
+        <div className="food-item-name-rating flex justify-between items-center mb-2">
+          <p className=" text-xl font-semibold">{item.name}</p>
+          <img src={assets.rating_starts} alt="rating" className=" max-w-20" />
+        </div>
+        <p className="food-item-desc text-gray-700 text-sm">
+          {item.description}
+        </p>
+        <p className="food-item-price text-orange-600 text-xl font-medium my-2">
+          ${item.price}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FoodItem;
