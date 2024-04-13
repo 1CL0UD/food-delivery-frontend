dce7ffdea997e287b54e3fa57e57a45e63fa9534
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-14 04:13:12 +0700

    added style

---
+++ src/components/FoodDisplay.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import { ContextValue, StoreContext } from '../providers/StoreProvider';
+import FoodItem from './FoodItem';
+
+interface Props {
+  category: string;
+}
+const FoodDisplay = ({ category }: Props) => {
+  const { food_list } = useContext(StoreContext) as ContextValue;
+  return (
+    <div className="food-display mt-8">
+      <h2 className=" text-2xl font-bold">Top Dishes ({category})</h2>
+      <div className="food-display-list grid grid-cols-4 mt-8 gap-8 gap-y-12">
+        {food_list.map((item) => {
+          return <FoodItem item={item} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FoodDisplay;
