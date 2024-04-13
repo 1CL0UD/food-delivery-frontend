28dc22dc16706cbd48e3891e832904870510768f
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-14 04:42:04 +0700

    added food display

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import ExploreMenu from '../../components/ExploreMenu';
 import Header from '../../components/Header';
+import FoodDisplay from '../../components/FoodDisplay';
 
 const Home = () => {
   const [category, setCategory] = useState('All');
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   );
 };
