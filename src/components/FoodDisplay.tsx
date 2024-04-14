ec4fdc010ba4084264c0029b8acd494bc50c7cda
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-14 10:05:54 +0700

    fixed animation

--- src/components/FoodDisplay.tsx
+++ src/components/FoodDisplay.tsx
@@ -11,9 +11,11 @@ const FoodDisplay = ({ category }: Props) => {
     <div className="food-display mt-8">
       <h2 className="text-2xl font-bold">Top Dishes ({category})</h2>
       <div className="food-display-list grid grid-cols-4 mt-8 gap-8 gap-y-12">
-        {food_list.map((item) => {
-          return <FoodItem key={item._id} item={item} />;
-        })}
+        {food_list.map((item) =>
+          category === 'All' || category === item.category ? (
+            <FoodItem key={item._id} item={item} />
+          ) : null
+        )}
       </div>
     </div>
   );
